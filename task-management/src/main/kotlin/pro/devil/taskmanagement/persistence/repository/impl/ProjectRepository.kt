39cba63a491183261e3358850a6943eca656a01a
Vyacheslav Tilikov <devil.ster.1@gmail.com>
2020-12-21 16:04:57 +0300

    Немного конфигурации логирования

--- task-management/src/main/kotlin/pro/devil/taskmanagement/persistence/repository/impl/ProjectRepository.kt
+++ task-management/src/main/kotlin/pro/devil/taskmanagement/persistence/repository/impl/ProjectRepository.kt
@@ -1,5 +1,7 @@
 package pro.devil.taskmanagement.persistence.repository.impl
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Repository
@@ -16,11 +18,16 @@ class ProjectRepository : IProjectRepository {
     @Value("\${project.suffix}")
     private var suffix: Int? = null
 
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(ProjectRepository::class.java)
+    }
+
     private val projects: MutableList<Project> = arrayListOf()
 
     override fun findById(id: Long?): Project? = projects.firstOrNull() { it.id == id }
 
     override fun save(project: Project): Project {
+        LOG.debug("Project Repository: Finding By Id {}", project.id)
         val existingProject = findById(project.id)
 
         updateInternalId(project)
